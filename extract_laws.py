5c2cdf171ac6453a09fc785ad25e5b5dd9bfa19c
sarahelmasryy <53651120+sarahelmasryy@users.noreply.github.com>
2023-02-25 19:44:06 +0100

    add cleaning function

--- extract_laws.py
+++ extract_laws.py
@@ -2,6 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import random
 from tqdm import tqdm
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+import re
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 years=list(range(1970, 2010))
@@ -21,7 +28,25 @@ for year in tqdm(years):
             for title in titles:
                 results.append(title.text)
             print(titles)
+            
+  
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('hungarian'))
+    filtered_tokens = [token for token in tokens if not token in stop_words]
+    clean_text = ' '.join(filtered_tokens)
+    return clean_text
+
+            
 
 with open("laws.txt", "w", encoding="utf-8") as txt_file:
     for line in results:
         txt_file.write("".join(line) + "\n")
+        
+        
+
